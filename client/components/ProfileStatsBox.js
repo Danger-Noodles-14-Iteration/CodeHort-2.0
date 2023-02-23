fcaef0f2f31ba9caa9598f6455db59e7a2122ad3
bkager <britta.ager@gmail.com>
2023-02-23 00:12:43 -0500

    Add working profile page components: ProfileStatsBox, ProfileStatsCard

--- client/components/ProfileStatsBox.js
+++ client/components/ProfileStatsBox.js
@@ -1,33 +1,41 @@
 import React from "react";
-import ProfileStatsCard from './ProfileStatsCard';
+import ProfileStatsCard from "./ProfileStatsCard";
 
+export default function ProfileStatsBox({ currUser }) {
+  console.log(currUser);
 
-export default function ProfileStatsBox({currUser}) {
-
-  console.log(currUser)
-  
   //create array of keys in currUser
   //iterate through array of keys; create new ProfileStatsCard for each
-    //pass it the currUser and key
+  //pass it the currUser and key
+  //Create array to hold newly created ProfileStatsCard elements; push each into array as created
+
+  let statsCardArray = [];
   let keysInUser = Object.keys(currUser);
-  
-  return(
+
+  for (let i = 0; i < keysInUser.length; i++) {
+    let key = keysInUser[i];
+    if (key === "username" || key === "password" || key === "isAdmin" || key === "__v" || key === "_id") continue;
+    let newCard = (
+      <ProfileStatsCard key={i} user={currUser} stat={currUser[key]} tracker={key} />
+    );
+    statsCardArray.push(newCard);
+  }
+
+  console.log("statsCardArray: ", statsCardArray);
+
+  // keysInUser.map((key) => {
+  //   if (key === 'username' || key === 'password' || key === 'isAdmin') continue;
+  //   let newCard = <ProfileStatsCard stat={currUser[key]} />;
+  //   statsCardArray.push(newCard);
+  // });
+
+  return (
     
+    <div className="flex flex-col items-center">
 
+      <h1>{props.user.username}</h1>
+      {statsCardArray}
+    </div>
   
-  <div className='flex flex-col items-center' >
-    <ProfileStatsCard currUser = {currUser}/>
-    <div className='cursor-pointer rounded-br-lg bg-gradient-to-bl from-fuchsia-900 via-gray-600 to-fuchsia-900 hover:bg-indigo-500 shadow-lg shadow-indigo-500/50 text-2xl font-extrabold ...text-white font-robotics bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500... hover:text-black w-fit p-4 border border-black m-2  hover:shadow-[0_4px_0px_rgb()] text-indigo bg-white ease-out hover:translate-y-1 transition-all rounded">
-    hover effect 1'
-    >
-      Name: {currUser.username}</div>
-    <div className='cursor-pointer rounded-br-lg bg-gradient-to-bl from-fuchsia-900 via-gray-600 to-fuchsia-900 hover:bg-indigo-500 shadow-lg shadow-indigo-500/50 text-2xl font-extrabold ...text-white font-robotics bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500... hover:text-black w-fit p-4 border border-black m-2  hover:shadow-[0_4px_0px_rgb()] text-indigo bg-white ease-out hover:translate-y-1 transition-all rounded">
-    hover effect 1'
-    > Cohort: {currUser.cohort}</div>
-    <div className='cursor-pointer rounded-br-lg bg-gradient-to-bl from-fuchsia-900 via-gray-600 to-fuchsia-900 hover:bg-indigo-500 shadow-lg shadow-indigo-500/50 text-2xl font-extrabold ...text-white font-robotics bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500... hover:text-black w-fit p-4 border border-black m-2  hover:shadow-[0_4px_0px_rgb()] text-indigo bg-white ease-out hover:translate-y-1 transition-all rounded">
-    hover effect 1'
-    > Participation Times: {currUser.participation}</div>
-  </div>
-)
-
+  );
 }
--- client/components/ProfileStatsCard.js
+++ client/components/ProfileStatsCard.js
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 
-export default function ProfileStatsCard({currUser}) {
-  console.log(currUser)
+
+export default function ProfileStatsCard(props) {
+
+  const [trackerStat, setTrackerStat] = useState();
+  console.log(props); 
+
+  // useEffect(async () => {
+  //   const trackerStat = fetch(`/user/add/${trackerName}`)
+  // }, [trackerStat])
+
+  // here we add a function to handle when a tracker's button is clicked; we will need to pass the tracker's name
+  async function handleTrackerIncr(trackerName) {
+    // we send a patch request to the proper extension; sending our user's username in the body
+    let res = await fetch(`/user/add/${trackerName}`,
+      {
+        method:'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username: currUser.username })
+      })
+    // we parse our response to log it, json is asynchronous so we await it
+    res = await res.json();
+    console.log('tracker incremented :', res)
+  }
+
+  // console.log(currUser)
+
 return(
   <div className='flex flex-col items-center' >
     {/* <div className='cursor-pointer rounded-br-lg bg-gradient-to-bl from-fuchsia-900 via-gray-600 to-fuchsia-900 hover:bg-indigo-500 shadow-lg shadow-indigo-500/50 text-2xl font-extrabold ...text-white font-robotics bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500... hover:text-black w-fit p-4 border border-black m-2  hover:shadow-[0_4px_0px_rgb()] text-indigo bg-white ease-out hover:translate-y-1 transition-all rounded">
@@ -15,10 +41,10 @@ return(
     <div className='cursor-pointer rounded-br-lg bg-gradient-to-bl from-fuchsia-900 via-gray-600 to-fuchsia-900 hover:bg-indigo-500 shadow-lg shadow-indigo-500/50 text-2xl font-extrabold ...text-white font-robotics bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500... hover:text-black w-fit p-4 border border-black m-2  hover:shadow-[0_4px_0px_rgb()] text-indigo bg-white ease-out hover:translate-y-1 transition-all rounded">
     hover effect 1'
     > Participation Times: {currUser.participation}</div> */}
-    <p>Test card</p>
+    <div className='cursor-pointer rounded-br-lg bg-gradient-to-bl from-fuchsia-900 via-gray-600 to-fuchsia-900 hover:bg-indigo-500 shadow-lg shadow-indigo-500/50 text-2xl font-extrabold ...text-white font-robotics bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500... hover:text-black w-fit p-4 border border-black m-2  hover:shadow-[0_4px_0px_rgb()] text-indigo bg-white ease-out hover:translate-y-1 transition-all rounded">
+    hover effect 1'>{props.tracker}: {trackerStat}</div>
+    <button onClick={() => handleTrackerIncr(props.tracker)}>+</button>
   </div>
-
-
 )
 
 }
