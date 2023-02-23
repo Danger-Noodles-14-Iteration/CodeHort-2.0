9d30eb3b8b9fc16fb4ea996c3f62af7ac84ac781
brianlim89 <br.lim89@gmail.com>
2023-02-22 18:19:01 -0800

    updated

--- client/components/PopupModal.js
+++ client/components/PopupModal.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const PopupModal = ({ setModalState }) => {
   return (
-    <div className='popup-modal'>
-      <div className="info-container">
+    <div className="popup-modal">
+      <div className="info-container dark:bg-slate-500">
         <header>
           <h2>Name</h2>
-          <button type='button' onClick={() => setModalState(false)}>X</button>
+          <button type="button" onClick={() => setModalState(false)}>
+            X
+          </button>
         </header>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default PopupModal
+export default PopupModal;
